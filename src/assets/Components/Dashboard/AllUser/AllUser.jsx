87040566885d2148668be2def9f978b9575c5ac9
humainislam11 <humainislam467@gmail.com>
2024-06-14 22:42:11 +0600

    load all user data

--- src/assets/Components/Dashboard/AllUser/AllUser.jsx
+++ src/assets/Components/Dashboard/AllUser/AllUser.jsx
@@ -1,11 +1,72 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { MdOutlineDelete } from "react-icons/md";
+import { FaUsers } from "react-icons/fa";
 
 
 const AllUser = () => {
+    const axiosSecure = useAxiosSecure();
+    const {data: users = []} = useQuery({
+        queryKey: ['users'],
+        queryFn: async () =>{
+            const res = await axiosSecure.get('/users');
+            return res.data;
+            
+        }
+        
+    })
+    const handleAdmin = user =>{
+
+    };
+
+    const handleDeleteUser = user =>{
+      
+    }
     return (
-        <div>
-             <div className="flex justify-evenly my-3 font-semibold">
-                <h1 className="text-3xl">All User</h1>
-                <h1 className="text-3xl">Total User</h1>
+        <div className=" ml-20 mt-20 bg-slate-300 p-9">
+             <div className="  my-3 font-semibold">
+              
+                <h1 className="text-3xl">Total User:{users.length}</h1>
+             </div>
+             <div>
+             <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead className="bg-lime-500">
+      <tr>
+        <th>Name</th>
+        <th>Email</th>
+        <th className="">Role</th>
+        <th className="">Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+      {
+        users.map(user => <tr key={user._id}>
+          
+        <td>{user?.name}</td>
+        <td>{user?.email}</td>
+        <tb className=''>
+          <button onClick={()=> handleAdmin(user)} className=" btn btn-lg bg-orange-500"><FaUsers  className="text-white"/></button>
+          </tb>
+        <td className="">
+        <button  onClick={ ()=> handleDeleteUser(user)} className="text-red-600 text-[20px]"><MdOutlineDelete /></button>
+        </td>
+       
+          </tr>
+         
+          
+          )
+      }
+     
+      
+    </tbody>
+    {/* foot */}
+    
+    
+  </table>
+</div>
              </div>
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -9,16 +9,21 @@ import {
 import AuthProvider from './assets/Components/AuthProvider/AuthProvider';
 import router from './Routers/Routers';
 import { HelmetProvider } from 'react-helmet-async';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 
-
+const  queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <HelmetProvider>
-   <AuthProvider>
+    <QueryClientProvider client={queryClient}>
+    <AuthProvider>
    <RouterProvider router={router} />
    </AuthProvider>
+
+    </QueryClientProvider>
+   
    </HelmetProvider>
   </React.StrictMode>,
 )
